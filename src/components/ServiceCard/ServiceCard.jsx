ec0c46ef5e2a6a23111c522acfba4d4b2c113a13
Yan Gutberg <yan_gutberg@DTECM0965071283.acad.unifor.br>
2023-12-21 17:48:25 -0300

    arjuste

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -5,7 +5,9 @@ import React from "react";
 const ServiceCard = ({ img, title, paragraph }) => {
   return (
     <div className="service-card-container">
+      <div className="service-card-image-container">
       <img className="service-card-image" src={img} alt="" />
+      </div>
       <div className="service-card-content">
         <h1 className="title">{title}</h1>
         <p className="paragraph">{paragraph}</p>
